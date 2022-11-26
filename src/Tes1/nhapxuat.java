60719e6fecfba67cc0409c72abb80e7dd635aff8
thnhtra <112228421+thnhtra@users.noreply.github.com>
2022-11-27 00:59:07 +0700

    nhap xuat ds sinh vien

---
+++ src/Tes1/nhapxuat.java
@@ -0,0 +1,9 @@
+package Tes1;
+
+public class nhapxuat {
+
+	public static void main(String[] args) {
+	        System.out.println("Hello World!");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author thanh
+ *
+ */
+module QTDL {
+}
